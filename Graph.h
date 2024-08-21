763cb258fad96e995b8fc0c8dc80b89e1fd9449a
Raynner Taniguchi <73478603+raynnertaniguchi@users.noreply.github.com>
2024-08-21 16:55:16 -0300

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -1,3 +1,10 @@
+/*
+* Trabalho Final - Algoritmo em Grafos (GCC218)
+* Sistemas de Informação - UFLA
+* Implementação Grupo R: Turma 14A
+* Patrícia Souza Couto, Matrícula: 202210524
+* Raynner Gabriel Taniguchi Silva Matrícula:202010167
+*/
 #ifndef GRAPH_H
 #define GRAPH_H
 
